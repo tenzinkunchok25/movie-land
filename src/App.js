c80cc69a680ed674e204704a4982a96f95d91517
Tenzin Kunchok <tenzinkunchok012511@gmial.com>
2024-03-05 11:57:20 -0500

    trying again

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+const API_URL = "http://www.omdbapi.com?apikey=f0c3a079";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies("Spider-man");
   }, []);
 
   const searchMovies = async (title) => {
